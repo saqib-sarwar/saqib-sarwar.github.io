a7faba7836c83cbdb5eb49904e54c36bc3bbc29a
saqib-sarwar <saqibsarwar@zhcet.ac.in>
2023-12-14 19:10:13 +0530

    random quote added

---
+++ scripts/quotations.js
@@ -0,0 +1,12 @@
+(function newQuote() {
+    var quotations = ['“Indeed, with hardship comes ease.”', 
+    '“And your Lord never forgets.”', 
+    '“Allah does not burden a soul beyond that it can bear.”',
+    '“The truth is from your Lord, so do not be among the doubters.”',
+    '“And Allah is with the doers of good.”',
+    '“If Allah should aid you, no one can overcome you; but if He should forsake you, who is there that can aid you after Him?”',
+    '“”'];
+    var randomQuote = Math.floor(Math.random()*quotations.length);
+    document.getElementById('quotation').innerHTML = quotations[randomQuote];
+
+  })();
