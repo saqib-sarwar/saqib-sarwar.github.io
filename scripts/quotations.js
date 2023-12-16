d80c06be93647f671500e81270b7c988b7758223
saqib-sarwar <saqibsarwar@zhcet.ac.in>
2023-12-16 15:21:45 +0530

    Long quote removed

--- scripts/quotations.js
+++ scripts/quotations.js
@@ -4,8 +4,8 @@
     '“Allah does not burden a soul beyond that it can bear.”',
     '“The truth is from your Lord, so do not be among the doubters.”',
     '“And Allah is with the doers of good.”',
-    '“If Allah should aid you, no one can overcome you; but if He should forsake you, who is there that can aid you after Him?”'
-    ];
+    // '“If Allah should aid you, no one can overcome you; but if He should forsake you, who is there that can aid you after Him?”',
+    '“And We test you with evil and with good as trial;”'];
     var randomQuote = Math.floor(Math.random()*quotations.length);
     document.getElementById('quotation').innerHTML = quotations[randomQuote];
 
