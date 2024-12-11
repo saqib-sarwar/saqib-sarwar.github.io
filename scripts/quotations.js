9ccb38a65436b31c8629c0e5049792b90bbbb7e6
saqib-sarwar <saqibsarwar@zhcet.ac.in>
2024-12-11 16:53:46 +0530

    Added workshop paper

--- scripts/quotations.js
+++ scripts/quotations.js
@@ -2,8 +2,9 @@
     var quotations = ['“Indeed, with hardship comes ease.”', 
     '“And your Lord never forgets.”', 
     '“Allah does not burden a soul beyond that it can bear.”',
-    '“The truth is from your Lord, so do not be among the doubters.”',
+    //'“The truth is from your Lord, so do not be among the doubters.”',
     '“And Allah is with the doers of good.”',
+    '"Discipline is the highest form of self-respect."',
     // '“If Allah should aid you, no one can overcome you; but if He should forsake you, who is there that can aid you after Him?”',
     '“And We test you with evil and with good as trial;”'];
     var randomQuote = Math.floor(Math.random()*quotations.length);
