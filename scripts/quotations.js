6b858700b5d9b463200453e5b617223b583c84a4
saqib-sarwar <saqibsarwar@zhcet.ac.in>
2024-12-26 16:13:00 +0530

    fixed quote

--- scripts/quotations.js
+++ scripts/quotations.js
@@ -1,13 +1,19 @@
-(function newQuote() {
-    var quotations = ['“Indeed, with hardship comes ease.”', 
-    '“And your Lord never forgets.”', 
-    '“Allah does not burden a soul beyond that it can bear.”',
-    //'“The truth is from your Lord, so do not be among the doubters.”',
-    '“And Allah is with the doers of good.”',
-    '"Discipline is the highest form of self-respect."',
-    // '“If Allah should aid you, no one can overcome you; but if He should forsake you, who is there that can aid you after Him?”',
-    '“And We test you with evil and with good as trial;”'];
-    var randomQuote = Math.floor(Math.random()*quotations.length);
-    document.getElementById('quotation').innerHTML = quotations[randomQuote];
 
-  })();
+(function newQuote() {
+  var quotations = [
+      '“Indeed, with hardship comes ease.” <span style="font-size: smaller; font-style: italic;">- Surah Ash-Sharh (The Relief)</span>', 
+      '“And your Lord never forgets.” <span style="font-size: smaller; font-style: italic;">- Surah Maryam (Mary)</span>', 
+      '“Allah does not burden a soul beyond that it can bear.” <span style="font-size: smaller; font-style: italic;">- Surah Al Baqarah (The Cow)</span>',
+      '“And Allah is with the doers of good.” <span style="font-size: smaller; font-style: italic;">- Surah Al Ankabut (The Spider)</span>',
+      '"Discipline is the highest form of self-respect."', 
+      '"The best among you are those who have the best manners and character." <span style="font-size: smaller; font-style: italic;">- Prophet Muhammad (PBUH)</span>',
+      '"Indeed, Allah is with those who are patient." <span style="font-size: smaller; font-style: italic;">- Surah Al Baqarah (The Cow)</span>',
+      '"The best of you are those who are best to their wives." <span style="font-size: smaller; font-style: italic;">- Prophet Muhammad (PBUH) (Al-Tirmidhi)</span>',
+      '"The best of you are those who are best to their families." <span style="font-size: smaller; font-style: italic;">- Prophet Muhammad (PBUH) (Mishkat)</span>',
+      '"The best of people are those who are most beneficial to people." <span style="font-size: smaller; font-style: italic;">- Prophet Muhammad (PBUH) (Tabarani)</span>',
+      '"The best of you are those who learn the Quran and teach it to others." <span style="font-size: smaller; font-style: italic;">- Prophet Muhammad (PBUH) (Al-Bukhari)</span>',
+      '“And We test you with evil and with good as trial;” <span style="font-size: smaller; font-style: italic;">- Surah Al Anbiya (The Prophets)</span>'
+  ];
+  var randomQuote = Math.floor(Math.random() * quotations.length);
+  document.getElementById('quotation').innerHTML = quotations[randomQuote];
+})();
